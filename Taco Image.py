1994cddafec4efbcaa5c93d6650d5da51b248034
yl7452ls <yl7452ls@go.minnstate.edu>
2019-12-11 11:37:08 -0600

    Adding a few comments

--- Taco Image.py
+++ Taco Image.py
@@ -1,9 +1,9 @@
-from PIL import Image, ImageFont, ImageDraw
-image = Image.open('jason-leung-tZq8NkZ3T8k-unsplash.jpg')
+from PIL import Image, ImageFont, ImageDraw     #The pillow libraries used to create the image
+image = Image.open('jason-leung-tZq8NkZ3T8k-unsplash.jpg')      #This is the image that will be edited
 
-img_draw = ImageDraw.Draw(image)
+img_draw = ImageDraw.Draw(image)    #I use the Draw library to create the text from the font library on the image.
 
-font = ImageFont.truetype('DejaVuSans.ttf', 500)
+font = ImageFont.truetype('DejaVuSans.ttf', 500)    #
 img_draw.text([350, 20], 'Random Taco Cookbook', fill='blue', font=font)
 image.thumbnail((800, 800))
 
