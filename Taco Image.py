c9098ba188c8cad1bc8d3d76e5a5a8b3f28247d1
yl7452ls <yl7452ls@go.minnstate.edu>
2019-12-11 11:54:53 -0600

    added blank at end for PEP reasons

--- Taco Image.py
+++ Taco Image.py
@@ -1,12 +1,12 @@
-from PIL import Image, ImageFont, ImageDraw     #The pillow libraries used to create the image
-image = Image.open('jason-leung-tZq8NkZ3T8k-unsplash.jpg')      #This is the image that will be edited
+from PIL import Image, ImageFont, ImageDraw     # The pillow libraries used to create the image
+image = Image.open('jason-leung-tZq8NkZ3T8k-unsplash.jpg')      # This is the image that will be edited
 
-img_draw = ImageDraw.Draw(image)    #I use the Draw library to create the text from the font library on the image.
+img_draw = ImageDraw.Draw(image)    # I use the Draw library to create the text from the font library on the image.
 
-font = ImageFont.truetype('DejaVuSans.ttf', 500)    #This is the font and the size of the font
-img_draw.text([350, 20], 'Random Taco Cookbook', fill='blue', font=font) #This is the font
-image.thumbnail((800, 800))
+font = ImageFont.truetype('DejaVuSans.ttf', 500)    # This is the font and the size of the font
+img_draw.text([350, 20], 'Random Taco Cookbook', fill='blue', font=font) # This is the font
+image.thumbnail((800, 800)) # This adjusts the size of the image while keeping that aspect ratio
 
-image.show()
+image.show() # This allows the image to show and view the modifications
 
-image.save('Omega Taco.jpg')
+image.save('Omega Taco.jpg') # Saves the image and creates a new taco: an Omega Taco :)
